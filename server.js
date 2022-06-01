ba0e474a959337410dc61c39f2ed9b09cbad510c
juan13ju <juan13ubaldo@gmail.com>
2022-06-01 13:59:17 -0500

    Agregue cookie parser y auth

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 // Importamos la funcion para conectarnos a la base de datos
 const {connection} = require("./src/config/db");
 // Importamos el router de usuarios
 const usuarios = require("./src/router/usuarios");
+const auth = require("./src/router/auth");
 // Configuracion usando dotemv
-const config = require("./src/config/config")
+const config = require("./src/config/config");
 const app = express();
 
 //Middleware
 app.use(express.json());
+app.use(cookieParser());
 
 //Nos conectamos a la base de datos
 connection();
 // Llamamos el router para usuarios
 usuarios(app);
+auth(app);
 
 // Inicializamos el servidor
 const server = app.listen(config.port);
 
 process.on('unhandledRejection',(err,promise)=>{
     console.log('Error',err.message)
+    console.log(err.stack);
     server.close(()=>process.exit(1))
 })
 
