84604224c089ab4fe6762e612865a6c84b1ffae1
juan13ju <juan13ubaldo@gmail.com>
2022-06-01 13:58:52 -0500

    Añadi secreto para jwt

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const config = {
     db_password : process.env.DB_PASSWORD,
-    port : process.env.PORT || 4000   
+    port : process.env.PORT || 4000,
+    jwt_secret : process.env.JWT_SECRET
 }
 
 module.exports = config;
