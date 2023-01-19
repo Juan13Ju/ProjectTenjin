c747a48bd11a2457447ea73aa10c3a0ce4e962db
juan13ju <juan13ubaldo@gmail.com>
2023-01-19 16:47:01 -0600

    Multer y Cloudinary para poder agregar foto de perfil

---
+++ src/libs/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+const config = require("../config/config")
+
+// Configuration 
+cloudinary.config({
+  cloud_name: config.cloudinary_cloud_key,
+  api_key: config.cloudinary_api_key,
+  api_secret: config.cloudinari_api_secret
+});
