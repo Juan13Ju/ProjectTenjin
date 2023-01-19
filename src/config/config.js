06540f8645daffb57a8d735eab3f94f7656f8710
juan13ju <juan13ubaldo@gmail.com>
2023-01-19 16:48:06 -0600

    Agrege llaves de cloudinary

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,10 @@ require("dotenv").config();
 const config = {
     db_password : process.env.DB_PASSWORD,
     port : process.env.PORT || 4000,
-    jwt_secret : process.env.JWT_SECRET
+    jwt_secret : process.env.JWT_SECRET,
+    cloudinary_cloud_key : process.env.CLOUDINARY_CLOUD_KEY,
+    cloudinary_api_key : process.env.CLOUDINARY_API_KEY,
+    cloudinari_api_secret : process.env.CLOUDINARY_API_SECRET
 }
 
 module.exports = config;
