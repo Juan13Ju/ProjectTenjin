49b607dc454aad27f9b90ee03660f76de48239f7
Juan13Ju <juan13ubaldo@gmail.com>
2023-05-08 22:43:59 -0600

    Primeros cambios para cambiar a bootstrap

--- public/js/main.js
+++ public/js/main.js
@@ -15,6 +15,52 @@ btnBuscar.addEventListener("click", () => {
     axios.get(url + materia).then(response => crearLista(response.data)).catch(err => console.log(err));
 });
 
+function createCard(userInfo, container){
+    let card = document.createElement("div");
+    card.classList.add("card", "col-md-4");
+    container.appendChild(card);
+    // La imagen de la tarjeta
+    let img = document.createElement("img");
+    img.classList.add("card-img-top")
+    img.setAttribute("src", "img/no-profile-image.png");
+    card.appendChild(img);
+    // Cuerpo de la tarjeta
+    let cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+    card.appendChild(cardBody);
+    // Nombre
+    let nombre = document.createElement("h5");
+    nombre.classList.add("card-title");
+    nombre.textContent = userInfo.nombre;
+    cardBody.appendChild(nombre);
+    // Info
+    let info = document.createElement("p");
+    info.classList.add("card-text");
+    info.textContent = userInfo.info;
+    cardBody.appendChild(info);
+    // Lista  
+    let cardUl = document.createElement("ul");
+    cardUl.classList.add("list-group", "list-group-flush");
+    card.appendChild(cardUl);
+    // Elementos de la lista
+    // Correo
+    let correo = document.createElement("li");
+    correo.textContent = userInfo.correo;
+    correo.classList.add("list-group-item");
+    cardUl.appendChild(correo);
+    // Carrera
+    let carrera = document.createElement("li");
+    carrera.textContent = userInfo.carrera;
+    carrera.classList.add("list-group-item");
+    cardUl.appendChild(carrera);
+    // Funcion para abrir una nueva ventana con la informacion del usuario
+    card.addEventListener("click", () => {
+        let correo = card.children[1].innerHTML;
+        window.location.href = "info.html?correo="+correo;
+       });
+
+}
+
 function crearLista(arr){
     // EL contenedor de la lista
     let contenedor = document.getElementById("listaAsesores");
@@ -24,68 +70,6 @@ function crearLista(arr){
         arr = arr.filter(el => el.carrera == filter.value);
     }
     arr.forEach(el => {
-        let card = document.createElement("div");
-        card.classList.add("card");
-        // Creamos un nuevo elemento para cada info que queremos mostrar
-       let nombre = document.createElement("h3");
-       nombre.textContent =  el.nombre;
-
-       let correo = document.createElement("h3");
-       correo.textContent = el.correo;
-       
-       let carrera = document.createElement("h3");
-       carrera.textContent = el.carrera;
-       
-       let info = document.createElement("p");
-       info.textContent = el.info;
-       
-       
-       // Etiquetas para la info
-       let etiqueta_nombre = document.createElement("h2");
-       etiqueta_nombre.textContent =  "Nombre";
-
-       let etiqueta_correo = document.createElement("h2");
-       etiqueta_correo.textContent =  "Correo";
-       
-       let etiqueta_carrera = document.createElement("h2");
-       etiqueta_carrera.textContent =  "Carrera de origen";
-
-       let etiqueta_info = document.createElement("h2");
-       etiqueta_info.textContent =  "Descripcion";
-    
-       
-       
-       let bloque1 = document.createElement("div");
-       bloque1.classList.add("sec-card");
-       let bloque2 = document.createElement("div");
-       bloque2.classList.add("sec-card");
-
-       let img = document.createElement("img");
-
-       card.append(bloque1);
-       card.append(bloque2);
-
-       bloque1.append(img);
-       img.setAttribute("src", "img/no-profile-image.png");
-
-       bloque1.append(etiqueta_nombre);
-       bloque1.append(nombre);
-
-       bloque1.append(etiqueta_correo);
-       bloque1.append(correo);
-       
-       bloque2.append(etiqueta_carrera);
-       bloque2.append(carrera);
-       
-       bloque2.append(etiqueta_info);
-       bloque2.append(info);
-       
-       // Funcion para abrir una nueva ventana con la informacion del usuario
-       card.addEventListener("click", () => {
-        let correo = card.children[1].innerHTML;
-        window.location.href = "info.html?correo="+correo;
-       });
-       // Añadimos el elemento al contenedor
-       contenedor.append(card);
+       createCard(el, contenedor);
     });
 }
